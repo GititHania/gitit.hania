aef48dcd45512bf8d4bd8b6e13b56c9c6fd4c9c5
GititHania <gitit.hania@gmail.com>
2021-11-28 15:09:12 +0200

    ex6

---
+++ cv.js
@@ -0,0 +1,28 @@
+function check_validtion(){
+    let phoneOK = document.getElementById("tel").checkValidity()
+    let phMsg = ""
+    if (phoneOK){
+        phMsg = "OK";
+    }
+    else{
+        phMsg = "Not OK";
+    }
+    document.getElementById("phoneVM").innerHTML = phMsg;
+    let emailOK = document.getElementById("EmailIn").checkValidity()
+    let emailMsg = ""
+    if (emailOK){
+        emailMsg = "OK";
+    }
+    else{
+        emailMsg = "Not OK";
+    }
+    document.getElementById("emailVM").innerHTML = emailMsg;
+    if (emailOK && phoneOK){
+        alert("Information OK!")
+    }
+}
+
+function next(){
+    window.location.href = "cv2.html";
+}
+
