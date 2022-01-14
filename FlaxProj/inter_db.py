9a8d33c3fbf160af220a3293fdc426005aee534d
GititHania <gitit.hania@gmail.com>
2022-01-14 13:22:16 +0200

    ex 11

--- FlaxProj/inter_db.py
+++ FlaxProj/inter_db.py
@@ -1,5 +1,5 @@
-from typing import NamedTuple
 import mysql.connector as mc
+import json
 
 
 def inter_db(query, query_type: str):
@@ -7,18 +7,21 @@ def inter_db(query, query_type: str):
     connection = mc.connect(
         host='localhost',
         user='root',
-        passwprd='root',
+        password='root',
         database='users'
     )
-    cursor = connection.cursor(named_tuple=True)
-    cursor.excute(query)
+    cursor = connection.cursor()
+    cursor.execute(query)
 
     if query_type == "commit":
         connection.commit()
-        return_value=True
+        return_value = True
     if query_type == 'fetch':
         query_result = cursor.fetchall()
-        return_value=query_result
+        return_value = query_result
+    if query_type == 'fetch_json':
+        query_result = json.dumps(cursor.fetchall())
+        return_value = query_result
 
     connection.close()
     cursor.close()
--- FlaxProj/pages/ex10/ex10.py
+++ FlaxProj/pages/ex10/ex10.py
@@ -1,11 +1,13 @@
 import flask as f
-from db_inter import inter_db
+from inter_db import inter_db
+import requests
 
 assignment10 = f.Blueprint(
     'assignment10',
     __name__,
     static_folder='static',
-    template_folder='tamplates'
+    template_folder='tamplates',
+    static_url_path='/static/'
 )
 
 
@@ -16,8 +18,10 @@ def insert(req):
 
     query = "insert into users (username, email, password) values('%s', '%s', '%s')" % (
         username, email, password)
-    inter_db(query, 'commit')
-    alertMsg = "A new user was inserted!"
+    if inter_db(query, 'commit'):
+        alertMsg = "A new user was inserted!"
+    else:
+        alertMsg = "No new user was inserted!"
     query_text = 'select * from users'
     users = inter_db(query_text, 'fetch')
     return f.render_template('Ex10Base.html', usersList=users, msg=alertMsg)
@@ -28,23 +32,22 @@ def update(req):
     new_us = req.form['up_name']
     new_em = req.form['up_email']
     new_pss = req.form['up_pass']
-    query ="UPDATE users SET username = '%s', email = '%s', password = '%s' WHERE email = '%s'" % (new_us, new_em, new_pss, old_email)
+    query = "UPDATE users SET username = '%s', email = '%s', password = '%s' WHERE email = '%s'" % (
+        new_us, new_em, new_pss, old_email)
     if inter_db(query, 'commit'):
         alertMsg = "User was updated!"
     else:
         alertMsg = "User not found"
-
     query_text = 'select * from users'
     users = inter_db(query_text, 'fetch')
     return f.render_template('Ex10Base.html', usersList=users, msg=alertMsg)
 
 
-
 def delete(req, users):
-    
+
     email = req.form['up_o_email']
     query = 'delete from users where email = '+email
-    if inter_db(query, 'commit'): 
+    if inter_db(query, 'commit'):
         alertMsg = "User was deleted!"
     else:
         alertMsg = "User not found"
@@ -69,3 +72,29 @@ def e10():
             delete(f.request)
 
     return f.render_template('Ex10Base.html', usersList=users)
+
+
+@assignment10.route('/assignment11')
+def ex11():
+    return f.redirect('/assignment11/users')
+
+
+@assignment10.route('/assignment11/users')
+def ex11_f():
+    query_text = 'select * from users'
+    users = inter_db(query_text, 'fetch_json')
+    return f.render_template('ex11.html', usersList=users)
+
+
+@assignment10.route('/assignment11/outer_source')
+def ex11_o():
+
+    if f.request.method == 'GET':
+        if 'backId' in f.request.args:
+            id = f.request.args['backId']
+            res = requests.get('https://reqres.in/api/users/'+ id)
+            user = res.json()
+
+            return f.render_template('ex11_o.html', selected = user['data'])
+
+    return f.render_template('ex11_o.html')
